1d99fd9b08f124e27e4099c790ebc19402ddde0b
Mark Murphy <mark.murphy@bigcommerce.com>
2024-03-14 15:57:50 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ git config --global --add safe.directory $GITHUB_WORKSPACE || exit 1
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 find ${GITHUB_WORKSPACE} -type f -name '*.mdx' -exec quality-docs {} + \
-  | reviewdog -f=markdown \
+  | reviewdog -f=remark-lint \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
